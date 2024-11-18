9a5960132642cc858aeb9191ec3ff4739842897d
valentin <val.pukhov@gmail.com>
2024-11-18 22:00:00 +0100

    Init

---
+++ src/main/kotlin/de/inphima/inphimabattlepass/InPhiMaBattlePassApplication.kt
@@ -0,0 +1,11 @@
+package de.inphima.inphimabattlepass
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InPhiMaBattlePassApplication
+
+fun main(args: Array<String>) {
+    runApplication<InPhiMaBattlePassApplication>(*args)
+}
---
+++ src/test/kotlin/de/inphima/inphimabattlepass/InPhiMaBattlePassApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.inphima.inphimabattlepass
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InPhiMaBattlePassApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
